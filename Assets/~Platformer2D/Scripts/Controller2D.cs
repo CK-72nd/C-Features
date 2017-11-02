d172eae0c98ae317c49f28ee28e4cf0f631db483
Calvin <calvinkeating72@gmail.com>
2017-11-02 16:04:10 +1100

    fjhjh

---
+++ Assets/~Platformer2D/Scripts/Controller2D.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer2D
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Controller2D : MonoBehaviour
+    {
+        public float accelerate = 20f;
+        public float jumpHeight = 10f;
+        public float rayDistance = 1f;
+        public LayerMask hitLayer;
+        public bool isGrounded = false;
+
+        private Rigidbody2D rigid2D;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid2D = GetComponent<Rigidbody2D>();
+        }
+
+        void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(transform.position, transform.position + -transform.up * rayDistance);
+        }
+
+        void FixedUpdate()
+        {
+            //Perform raycast down
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, -transform.up, rayDistance, hitLayer);
+            //If hit something (hit.collider !=null)
+            if (hit.collider != null)
+            {
+                //isGrounded
+                isGrounded = true;
+            }
+            //else
+            else
+            {
+                //isGrounded = false
+                isGrounded = false;
+            }
+        }
+
+        public void Jump()
+        {
+            rigid2D.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        }
+
+        public void Move(float inputH)
+        {
+            rigid2D.AddForce(transform.right * inputH * accelerate);
+        }
+
+    }
+}
---
+++ Assets/~Platformer2D/Scripts/UserInput2D.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer2D
+{
+    //Forces unity to add 'Controller2D' component to
+    //the same object as 'UserInput2D as soon as
+    //you attach this script to a GameObject
+
+    [RequireComponent(typeof(Controller2D))]
+    public class UserInput2D : MonoBehaviour
+    {
+        //Variable to store the controller2D component
+        private Controller2D controller;
+
+        void Awake()
+        {
+            controller = GetComponent<Controller2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Get input on the horizontal axis (A or D)
+            float inputH = Input.GetAxis("Horizontal");
+            //Move the controller using input axis
+            controller.Move(inputH);
+            // Check if space is down
+            if(Input.GetKeyDown(KeyCode.Space)&& controller.isGrounded)
+            {
+                //Get player to jump 
+                controller.Jump();
+            }
+        }
+    }
+}
